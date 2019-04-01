cace57f86cb19a6aef7d3137b12bf0698553444a
minus31 <donotgetgreed@gmail.com>
2019-04-01 14:30:41 +0900

    modify crawler

--- sub_crawler.py
+++ sub_crawler.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 def login(driver, login_info):
     url = "https://www.musinsa.com/"
+    driver.get(url)
 
     driver.find_element_by_css_selector(
         "#wrapper > div.top-column.column > div > div.userMenu-wrapper.toggleBox > ul > li.listItem.loginBtn > a").click()
@@ -16,22 +17,30 @@ def login(driver, login_info):
     return True
 
 
-def get_item_img(spec_url, login_info):
+def get_item_img(spec_url, login_info=None, close=False):
     driver = webdriver.Chrome()
     if login_info:
         login(driver, login_info)
 
     driver.get(spec_url)
 
-    related_imgs_urls = []
+    related_items_urls = []
 
-    related_imgs = driver.find_elements_by_css_selector(
-        "div .storeListBox > ul > li.listItem > div.articleImg")
+    related_items = driver.find_elements_by_css_selector(
+        r"div > ul.styleItem-list > li.listItem")
 
-    for img in related_imgs:
+    for block in related_items:
+        related_items = block.find_elements_by_css_selector("div.articleImg")
+        block_imgs = []
+        for item in related_items:
 
-        related_imgs_url = img.find_element_by_css_selector(
-            "a").get_attribute('href')
-        related_imgs_urls.append(related_imgs_url)
+            item_url = item.find_element_by_css_selector(
+                "a").get_attribute('href')
+            block_imgs.append(item_url)
 
-    return related_imgs_urls
+        related_items_urls.append(block_imgs)
+        
+    if close:
+        driver.close()
+
+    return related_items_urls
