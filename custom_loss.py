12fd9d991e844a1bda4166150e0a941dadc7a1e1
minus31 <donotgetgreed@gmail.com>
2019-04-19 11:32:56 +0900

    modify loss

--- custom_loss.py
+++ custom_loss.py
@@ -11,23 +11,37 @@ def ArcFaceloss(labels, features):
 
     N = tf.shape(labels)[0]
     s = 64.
-    m1 = 0.9
-    m2 = 0.4
-    m3 = 0.15
+    m = 0.5
+    
+    cos_m = math.cos(m)
+    sin_m = math.sin(m)
+    mm = sin_m * m  
+    threshold = math.cos(math.pi - m)
+    
+    cos_t2 = tf.square(features, name='cos_2')
+    sin_t2 = tf.subtract(1., cos_t2, name='sin_2')
+    sin_t = tf.sqrt(sin_t2, name='sin_t')
+    cos_mt = s * tf.subtract(tf.multiply(cos_t, cos_m), tf.multiply(sin_t, sin_m), name='cos_mt')
 
-    target_cos = tf.reduce_sum(tf.cast(labels, tf.float32) * features, axis=-1)
-    target_cos = tf.cos(tf.math.acos(target_cos) * m1 + m2) - m3
-    target_cos = tf.exp(s * target_cos)
+    # this condition controls the theta+m should in range [0, pi]
+    #      0<=theta+m<=pi
+    #     -m<=theta<=pi-m
+    cond_v = cos_t - threshold
+    cond = tf.cast(tf.nn.relu(cond_v, name='if_else'), dtype=tf.bool)
 
-    others = -1. * tf.multiply(tf.subtract(tf.cast(labels, tf.float32), 1.0), features)
-    others = tf.exp(s * others)
-    others = tf.subtract(tf.reduce_sum(others, axis=-1), 1.0)
+    keep_val = s*(cos_t - mm)
+    cos_mt_temp = tf.where(cond, cos_mt, keep_val)
 
-    log_ = tf.log(tf.divide(target_cos, tf.add(tf.add(target_cos, others), 1e-5)))
+    mask = tf.cast(labels, tf.float32)
+    inv_mask = tf.subtract(1., mask, name='inverse_mask')
 
-    output = -1. * tf.divide(tf.reduce_sum(log_), tf.cast(N, tf.float32))
+    s_cos_t = tf.multiply(s, cos_t, name='scalar_cos_t')
 
-    return output
+    logit = tf.add(tf.multiply(s_cos_t, inv_mask), tf.multiply(cos_mt_temp, mask), name='arcface_loss_output')
+    
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits=logit, labels=labels)
+
+    return tf.reduce_mean(loss)
 
 
 if __name__ == '__main__':
